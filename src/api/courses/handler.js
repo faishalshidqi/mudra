f70b17b37ce5118cee028a34e6802a3a183e7e01
faishalshidqi <rshidqi6@gmail.com>
2023-05-04 20:35:33 +0700

    replacing bind with a arrow functions

--- src/api/courses/handler.js
+++ src/api/courses/handler.js
@@ -5,9 +5,6 @@ class CoursesHandler {
 	constructor(service, /*validator*/) {
 		this._service = service
 		//this._validator = validator
-
-		this.getCoursesHandler = this.getCoursesHandler.bind(this)
-		this.getCourseByIdHandler = this.getCourseByIdHandler.bind(this)
 	}
 
 	async getCoursesHandler() {
