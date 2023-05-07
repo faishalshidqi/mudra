022ba89dfff198b654e2b6e670cc536a481ac63d
faishalshidqi <rshidqi6@gmail.com>
2023-05-07 12:33:37 +0700

    Revert "replacing bind with a arrow functions"
    This reverts commit f70b17b37ce5118cee028a34e6802a3a183e7e01.

--- src/api/courses/handler.js
+++ src/api/courses/handler.js
@@ -5,6 +5,9 @@ class CoursesHandler {
 	constructor(service, /*validator*/) {
 		this._service = service
 		//this._validator = validator
+
+		this.getCoursesHandler = this.getCoursesHandler.bind(this)
+		this.getCourseByIdHandler = this.getCourseByIdHandler.bind(this)
 	}
 
 	/*async postCourseHandler(request, h){
--- src/api/courses/routes.js
+++ src/api/courses/routes.js
@@ -2,18 +2,13 @@ const routes = (handler) => [
 	{
 		method: 'GET',
 		path: '/courses',
-		handler: () => handler.getCoursesHandler()
+		handler: (request, h) => handler.getCoursesHandler(request, h)
 	},
 	{
 		method: 'GET',
 		path: '/courses/{id}',
 		handler: (request, h) => handler.getCourseByIdHandler(request, h)
 	},
-	{
-		method: 'POST',
-		path: '/courses',
-		handler: (request, h) => handler.postCourseHandler(request, h)
-	}
 ]
 
 module.exports = routes
