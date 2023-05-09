e664a35994fa3dfac82199bac69555b3e7271c60
faishalshidqi <rshidqi6@gmail.com>
2023-05-09 11:01:18 +0700

    added validator and an update for course management

--- src/validator/course_managers/schema.js
+++ src/validator/course_managers/schema.js
@@ -1,7 +1,6 @@
 const Joi = require('joi')
 
 const CourseManagerPayloadSchema = Joi.object({
-    //{title, sign_pict_link, description, type, is_deleted}
     title: Joi.string().required(),
     sign_pict_link: Joi.string().required(),
     description: Joi.string().required(),
