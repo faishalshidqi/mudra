c4f8b89c2c406d7be6d66a8871b99b33f33baef5
faishalshidqi <rshidqi6@gmail.com>
2023-05-12 07:28:18 +0700

    fixed some config

--- src/api/managers/handler.js
+++ src/api/managers/handler.js
@@ -1,146 +1,146 @@
-const ClientError = require("../../exceptions/ClientError");
+const ClientError = require('../../exceptions/ClientError')
 
 class ManagersHandler {
-    constructor(service, validator) {
-        this._service = service
-        this._validator = validator
-    }
+	constructor(service, validator) {
+		this._service = service
+		this._validator = validator
+	}
 
-    async postManagedCourseHandler(request, h){
-        try {
-            this._validator.validateCourseManagerPayload(request.payload)
+	async postManagedCourseHandler(request, h){
+		try {
+			this._validator.validateCourseManagerPayload(request.payload)
 
-            const {title, sign_pict_link, description, type, is_deleted} = request.payload
-            const course_id = await this._service.addManagedCourse({
-                title, sign_pict_link, description, type, is_deleted
-            })
-            const response = h.response({
-                status: 'success',
-                message: 'Course is added successfully',
-                data: {
-                    course_id
-                }
-            })
-            response.code(201)
-            return response
-        }
-        catch (error) {
-            if (error instanceof ClientError) {
-                const response = h.response({
-                    status: 'failed',
-                    message: error.message
-                })
-                response.code(error.statusCode)
-                return response
-            }
-            const response = h.response({
-                status: 'error',
-                // message: 'Sorry for the inconvenience, our server is having an error.'
-                message: error.message
-            })
-            response.code(500)
-            return response
-        }
-    }
+			const {title, sign_pict_link, description, type, is_deleted} = request.payload
+			const course_id = await this._service.addManagedCourse({
+				title, sign_pict_link, description, type, is_deleted
+			})
+			const response = h.response({
+				status: 'success',
+				message: 'Course is added successfully',
+				data: {
+					course_id
+				}
+			})
+			response.code(201)
+			return response
+		}
+		catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: 'failed',
+					message: error.message
+				})
+				response.code(error.statusCode)
+				return response
+			}
+			const response = h.response({
+				status: 'error',
+				// message: 'Sorry for the inconvenience, our server is having an error.'
+				message: error.message
+			})
+			response.code(500)
+			return response
+		}
+	}
 
-    async getManagedCoursesHandler(){
-        const courses = (await this._service.getManagedCourses()).rows
-        return {
-            status: 'success',
-            data: {
-                courses
-            }
-        }
-    }
+	async getManagedCoursesHandler(){
+		const courses = (await this._service.getManagedCourses()).rows
+		return {
+			status: 'success',
+			data: {
+				courses
+			}
+		}
+	}
 
-    async getManagedCourseByIdHandler(request, h){
-        try {
-            const {id} = request.params
-            const course = ((await this._service.getManagedCourseById(id)).rows)[0]
-            return {
-                status: 'success',
-                data: {
-                    course
-                }
-            }
-        }
-        catch (error) {
-            if (error instanceof ClientError) {
-                const response = h.response({
-                    status: 'failed',
-                    message: error.message
-                })
-                response.code(error.statusCode)
-                return response
-            }
-            const response = h.response({
-                status: 'error',
-                message: 'Sorry for the inconvenience, our server is having an error.'
-            })
-            response.code(500)
-            console.error(error)
-            return response
-        }
-    }
+	async getManagedCourseByIdHandler(request, h){
+		try {
+			const {id} = request.params
+			const course = ((await this._service.getManagedCourseById(id)).rows)[0]
+			return {
+				status: 'success',
+				data: {
+					course
+				}
+			}
+		}
+		catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: 'failed',
+					message: error.message
+				})
+				response.code(error.statusCode)
+				return response
+			}
+			const response = h.response({
+				status: 'error',
+				message: 'Sorry for the inconvenience, our server is having an error.'
+			})
+			response.code(500)
+			console.error(error)
+			return response
+		}
+	}
 
-    async editCourseByIdHandler(request, h) {
-        try {
-            this._validator.validateCourseManagerPayload(request.payload)
-            const {id} = request.params
+	async editCourseByIdHandler(request, h) {
+		try {
+			this._validator.validateCourseManagerPayload(request.payload)
+			const {id} = request.params
 
-            await this._service.editManagedCourseById(id, request.payload)
-            return {
-                status: 'success',
-                message: 'Course is successfully altered'
-            }
-        }
-        catch (error) {
-            if (error instanceof ClientError) {
-                const response = h.response({
-                    status: 'failed',
-                    message: error.message
-                })
-                response.code(error.statusCode)
-                return response
-            }
-            const response = h.response({
-                status: 'error',
-                message: 'Sorry for the inconvenience, our server is having an error.'
-            })
-            response.code(500)
-            console.error(error)
-            return response
-        }
-    }
+			await this._service.editManagedCourseById(id, request.payload)
+			return {
+				status: 'success',
+				message: 'Course is successfully altered'
+			}
+		}
+		catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: 'failed',
+					message: error.message
+				})
+				response.code(error.statusCode)
+				return response
+			}
+			const response = h.response({
+				status: 'error',
+				message: 'Sorry for the inconvenience, our server is having an error.'
+			})
+			response.code(500)
+			console.error(error)
+			return response
+		}
+	}
 
-    async deleteCourseByIdHandler(request, h){
-        try {
-            const {id} = request.params
+	async deleteCourseByIdHandler(request, h){
+		try {
+			const {id} = request.params
 
-            await this._service.deleteManagedCourseById(id)
-            return {
-                status: 'success',
-                message: 'Course is successfully removed'
-            }
-        }
-        catch (error) {
-            if (error instanceof ClientError) {
-                const response = h.response({
-                    status: 'failed',
-                    message: error.message
-                })
-                response.code(error.statusCode)
-                return response
-            }
-            const response = h.response({
-                status: 'error',
-                message: 'Sorry for the inconvenience, our server is having an error.'
-            })
-            response.code(500)
-            console.error(error)
-            return response
-        }
-    }
+			await this._service.deleteManagedCourseById(id)
+			return {
+				status: 'success',
+				message: 'Course is successfully removed'
+			}
+		}
+		catch (error) {
+			if (error instanceof ClientError) {
+				const response = h.response({
+					status: 'failed',
+					message: error.message
+				})
+				response.code(error.statusCode)
+				return response
+			}
+			const response = h.response({
+				status: 'error',
+				message: 'Sorry for the inconvenience, our server is having an error.'
+			})
+			response.code(500)
+			console.error(error)
+			return response
+		}
+	}
 }
 
 module.exports = ManagersHandler
--- src/services/postgres/CoursesService.js
+++ src/services/postgres/CoursesService.js
@@ -3,34 +3,34 @@ const NotFoundError = require('../../exceptions/NotFoundError')
 const { mapDBToModelCourses } = require('../../utils/mapDBToModel')
 
 class CoursesService {
-  constructor() {
-    this._pool = new Pool()
-  }
+	constructor() {
+		this._pool = new Pool()
+	}
 
-  async getCourses() {
-    const query = {
-      text: 'select * from courses'
-    }
-    const result = await this._pool.query(query)
+	async getCourses() {
+		const query = {
+			text: 'select * from courses where is_deleted = false'
+		}
+		const result = await this._pool.query(query)
 
-    if (!result.rowCount) {
-      throw new NotFoundError('Courses tidak ditemukan')
-    }
-    return result.rows.map(mapDBToModelCourses)
-  }
+		if (!result.rowCount) {
+			throw new NotFoundError('Courses tidak ditemukan')
+		}
+		return result.rows.map(mapDBToModelCourses)
+	}
 
-  async getCourseById(id) {
-    const query = {
-      text: 'select * from courses where course_id = $1',
-      values: [id]
-    }
-    const result = await this._pool.query(query)
+	async getCourseById(id) {
+		const query = {
+			text: 'select * from courses where course_id = $1 and is_deleted = false',
+			values: [id]
+		}
+		const result = await this._pool.query(query)
 
-    if (!result.rowCount) {
-      throw new NotFoundError('Course tidak ditemukan')
-    }
-    return result.rows.map(mapDBToModelCourses)[0]
-  }
+		if (!result.rowCount) {
+			throw new NotFoundError('Course tidak ditemukan')
+		}
+		return result.rows.map(mapDBToModelCourses)[0]
+	}
 }
 
 module.exports = CoursesService
