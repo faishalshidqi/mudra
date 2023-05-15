01fdf8b0877fe652879b46669adef0e867cdbdcb
faishalshidqi <rshidqi6@gmail.com>
2023-05-15 18:43:27 +0700

    added changes

--- src/api/managers/routes.js
+++ src/api/managers/routes.js
@@ -17,12 +17,37 @@ const routes = (handler) => [
 	{
 		method: 'PUT',
 		path: '/kll/courses/{id}',
-		handler: (request, h) => handler.editCourseByIdHandler(request, h)
+		handler: (request, h) => handler.editManagedCourseByIdHandler(request, h)
 	},
 	{
 		method: 'DELETE',
 		path: '/kll/courses/{id}',
-		handler: (request, h) => handler.deleteCourseByIdHandler(request, h)
+		handler: (request, h) => handler.deleteManagedCourseByIdHandler(request, h)
+	},
+	{
+		method: 'POST',
+		path: '/kll/challenges',
+		handler: (request, h) => handler.postManagedChallengeHandler(request, h)
+	},
+	{
+		method: 'GET',
+		path: '/kll/challenges',
+		handler: (request, h) => handler.getManagedChallengesHandler(request, h)
+	},
+	{
+		method: 'GET',
+		path: '/kll/challenges/{id}',
+		handler: (request, h) => handler.getManagedChallengeByIdHandler(request, h)
+	},
+	{
+		method: 'PUT',
+		path: '/kll/challenges/{id}',
+		handler: (request, h) => handler.editManagedChallengeByIdHandler(request, h)
+	},
+	{
+		method: 'DELETE',
+		path: '/kll/challenges/{id}',
+		handler: (request, h) => handler.deleteManagedChallengeByIdHandler(request, h)
 	}
 ]
 
