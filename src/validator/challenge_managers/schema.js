655cbb76da007a3e8da3d03c7ebf3ba5e92a0836
faishalshidqi <rshidqi6@gmail.com>
2023-05-16 15:53:51 +0700

    fixed validator schema for challenges management

--- src/validator/challenge_managers/schema.js
+++ src/validator/challenge_managers/schema.js
@@ -1,13 +1,12 @@
 const Joi = require('joi')
 
 const ChallengeManagerPayloadSchema = Joi.object({
-	// {title, description, course_id, answer, is_deleted, type}
 	title: Joi.string().required(),
 	description: Joi.string().required(),
 	course_id: Joi.string().required(),
 	answer: Joi.string().required(),
-	is_deleted: Joi.string().required(),
-	type: Joi.boolean().required()
+	is_deleted: Joi.boolean().required(),
+	type: Joi.string().required()
 })
 
 module.exports = {ChallengeManagerPayloadSchema}
