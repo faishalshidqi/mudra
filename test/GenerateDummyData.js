8dd9e9997051f0e0aafebf7530859b987f4b6d1f
Zatfley <alfi.dim1612@gmail.com>
2023-05-18 17:44:19 +0700

    change dummy generator query

--- test/GenerateDummyData.js
+++ test/GenerateDummyData.js
@@ -7,10 +7,8 @@ const pool = new Pool()
 const GenearateDummyData = async () => {
 
     console.log('generate data...')
-    await Promise.all([
-        generateCourseData(),
-        generateChallengesData()
-    ]);
+    await generateCourseData()
+    await generateChallengesData()
 
     console.log('semua dummy data berhasil dibuat')
     return 'menghentikkan process'
@@ -19,7 +17,7 @@ const GenearateDummyData = async () => {
 const generateCourseData = async () => {
     const values = []
     for (let i = 0; i < 5; i++) {
-        const v = [`co-${i}`, `dummy`, `dummy`, `dummy`, `dummy`, `dummy`, false]
+        const v = [`co-${i}`, `dummy`, `dummy`, `dummy`, `dummy`, new Date().toISOString(),new Date().toISOString(), false]
         values.push(v)
     }
     await pool.query(format('INSERT INTO courses VALUES %L', values), [])
@@ -29,7 +27,7 @@ const generateCourseData = async () => {
 const generateChallengesData = async () => {
     const values = []
     for (let i = 0; i < 5; i++) {
-        const v = [`chg-${i}`, `dummy`, `dummy`, `co-${i}`, `d`, false]
+        const v = [`chg-${i}`, `dummy`, `dummy`, `co-${i}`, `d`, new Date().toISOString(), new Date().toISOString(), false, `dummy`]
         values.push(v)
     }
     await pool.query(format('INSERT INTO challenges VALUES %L', values), [])
