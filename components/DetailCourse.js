745644d4af60ea2492f05d512726cd9a74e15b00
Zatfley <alfi.dim1612@gmail.com>
2023-05-30 19:51:22 +0700

    update swal handler

--- components/DetailCourse.js
+++ components/DetailCourse.js
@@ -37,8 +37,6 @@ export default function DetailCourse({ courseData: {id, createdAt, description, 
                             icon: "error"
                         })
                     });
-            } else if (result.isDenied) {
-                console.log('No')
             }
         })
     }
