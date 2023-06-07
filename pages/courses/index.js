2054283b6d2d2802fff0fa997b32a04f096fb8d5
Zatfley <alfi.dim1612@gmail.com>
2023-06-07 20:49:37 +0700

    first migrate to SWR

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -5,27 +5,31 @@ import NavigationItem from "../../components/NavigationItem"
 import Navigation from "../../components/Navigation"
 import fetchApi from "../../lib/FetchApi"
 import {useRouter} from "next/router"
+import useSWR from 'swr'
+import Custom404 from "../../components/Custom404";
+import Loading from "../../components/Loading";
 
-export async function getStaticProps() {
-	const courseData = await fetchApi.getAllCourses()
-	return {
-		props: {
-			courseData
-		}
-	}
-}
-export default function CoursesList({ courseData }) {
+// export async function getStaticProps() {
+// 	// const courseData = await fetchApi.getAllCourses()
+//
+// 	return {
+// 		props: {
+// 			courseData
+// 		}
+// 	}
+// }
+export default function CoursesList() {
+	const { data, error, isLoading } = useSWR(`${process.env.API_URL}/kll/courses`, fetchApi.getAllCourses)
+
 	const router = useRouter()
-	if(router.isFallback) {
+	if (isLoading) {
 		return (
-			<div
-				className="m-56 inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-				role="status">
-				<span
-					className=" !absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
-                      Loading...
-				</span>
-			</div>
+			<Loading />
+		)
+	}
+	if (!data || error) {
+		return (
+			<Custom404 message="Can't found any Courses data"></Custom404>
 		)
 	}
 	return (
@@ -38,7 +42,7 @@ export default function CoursesList({ courseData }) {
 				<NavigationItem href='/challenges/form'>Add New Challenge</NavigationItem>
 			</Navigation>
 			<List>
-				{courseData["courses"].map((data) => (
+				{data?.courses.map((data) => (
 					<ListItem key={data.course_id} context={data} />
 				))}
 			</List>
