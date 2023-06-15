1f8d94c0e17cc94fb91e3ace99172a1d5b07c7ac
faishalshidqi <rshidqi6@gmail.com>
2023-06-15 15:15:39 +0700

    fixed build process

--- components/CourseForm.js
+++ components/CourseForm.js
@@ -189,15 +189,14 @@ export default function CourseForm({courseData}) {
 							</div>
 						</div>
 						<div className="sm:col-span-full">
-							<label htmlFor="type"
-								   className="block text-sm font-medium leading-6">Course Type</label>
+							<label htmlFor="type" className="block text-sm font-medium leading-6">Course Type</label>
 							<div className="mt-2">
 								<select id="type"
-										name="type"
-										autoComplete="course-type"
-										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-										onChange={handleOptionValueChange}
-										value={selectedOption}
+									name="type"
+									autoComplete="course-type"
+									className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+									onChange={handleOptionValueChange}
+									value={selectedOption}
 								>
 									<option value='Default' disabled>Choose Course Type</option>
 									<option value='SIBI'>SIBI</option>
